4b780201ec0e90d8e86ca7c44c14f48a6dfc5e27
Parrot <parrot@localhost.localdomain>
2025-02-18 11:18:07 +0100

    18_02_25

--- src/main.c
+++ src/main.c
@@ -27,6 +27,60 @@ int	unit_circle(float angle, char c)
 	return (0);
 }
 
+void	print_error_code(t_parsing *parsing_info)
+{
+	if (parsing_info->error_code == OK)
+		return ;
+	printf("Error\n");
+	if (parsing_info->error_code == I_C)
+		printf("Invalid character or invalid line.\n");
+	else if (parsing_info->error_code == M_C_I_M)
+		printf("Invalid color.\n");
+	else if (parsing_info->error_code == M_T_I_M)
+		printf("Invalid texture.\n");
+	else if (parsing_info->error_code == P_A_S)
+		printf("Player already set.\n");
+	else if (parsing_info->error_code == P_N_S)
+		printf("Player not set.\n");
+	else if (parsing_info->error_code == M_N_C)
+		printf("Invalid map content.\n");
+	else if (parsing_info->error_code == L_O_S)
+		printf("Line of spaces only.\n");
+	else if (parsing_info->error_code == E_M)
+		printf("Empty map.\n");
+	else
+		printf("Unknown error code.\n");
+}
+
+void	free_parsing_info(t_parsing *parsing_info)
+{
+	int	i;
+
+	if (parsing_info->map)
+	{
+		i = 0;
+		while (parsing_info->map[i])
+		{
+			free(parsing_info->map[i]);
+			i++;
+		}
+		free(parsing_info->map);
+		parsing_info->map = NULL;
+	}
+	if (parsing_info->NO_TEXTURE)
+		free(parsing_info->NO_TEXTURE);
+	if (parsing_info->SO_TEXTURE)
+		free(parsing_info->SO_TEXTURE);
+	if (parsing_info->WE_TEXTURE)
+		free(parsing_info->WE_TEXTURE);
+	if (parsing_info->EA_TEXTURE)
+		free(parsing_info->EA_TEXTURE);
+	parsing_info->NO_TEXTURE = NULL;
+	parsing_info->SO_TEXTURE = NULL;
+	parsing_info->WE_TEXTURE = NULL;
+	parsing_info->EA_TEXTURE = NULL;
+}
+
 int	main(int argc, char *argv[])
 {
 	t_game		*game;
@@ -35,7 +89,12 @@ int	main(int argc, char *argv[])
 	print_title();
 	parsing_info = malloc(sizeof(t_parsing));
 	if (parsing(argc, argv, parsing_info))
+	{
+		print_error_code(parsing_info);
+		free_parsing_info(parsing_info);
+		free(parsing_info);
 		return (1);
+	}
 	init_game(&game, parsing_info);
 	print_game_instance(game);
 	if (parsing_info->player_letter == 'N')
@@ -49,5 +108,7 @@ int	main(int argc, char *argv[])
 	init_hooks(game);
 	print_keys();
 	mlx_loop(game->mlx_pointer);
+	free_parsing_info(parsing_info);
+	free(parsing_info);
 	return (0);
 }
